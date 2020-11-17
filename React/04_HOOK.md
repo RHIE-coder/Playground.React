b27f8a146fed639650dc283ec92436331661d8d6
MinHyungRHIE <quotia72@naver.com>
2020-11-17 23:41:54 +0900

    react ... difficult

---
+++ React/04_HOOK.md
@@ -0,0 +1,47 @@
+# 🌀1. Hook 소개
+
+<br><br><br><br><br>
+<br><br><br><br><br>
+<br><br><br><br><br>
+
+# 🌀2. Hook 개요
+
+<br><br><br><br><br>
+<br><br><br><br><br>
+<br><br><br><br><br>
+
+# 🌀3. State Hook 사용하기
+
+<br><br><br><br><br>
+<br><br><br><br><br>
+<br><br><br><br><br>
+
+# 🌀4. Effect Hook 사용하기
+
+<br><br><br><br><br>
+<br><br><br><br><br>
+<br><br><br><br><br>
+
+# 🌀5. Hook 규칙
+
+<br><br><br><br><br>
+<br><br><br><br><br>
+<br><br><br><br><br>
+
+# 🌀6. 자신만의 Hook 만들기
+
+<br><br><br><br><br>
+<br><br><br><br><br>
+<br><br><br><br><br>
+
+# 🌀7. Hook API 참고서
+
+<br><br><br><br><br>
+<br><br><br><br><br>
+<br><br><br><br><br>
+
+# 🌀8. Hook 자주 묻는 질문
+
+<br><br><br><br><br>
+<br><br><br><br><br>
+<br><br><br><br><br>
--- SandBox/React/script.js
+++ SandBox/React/script.js
@@ -1,7 +1,35 @@
-const name = 'Josh Perez';
-const element = <h1>Hello, {name}</h1>;
+class NameForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
 
 ReactDOM.render(
-    element,
+    <NameForm />,
     document.getElementById('root')
 );
